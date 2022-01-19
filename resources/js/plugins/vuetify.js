617a5ffdba2b6a737c0551b7ac7c597ccfb065e1
neondoll <bonni_1998@mail.ru>
2022-01-19 14:15:07 +0300

    n chip

--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -12,13 +12,14 @@ const opts = {
     theme: {
         themes: {
             light: {
-                primary: '#007BFF',
-                secondary: '#424242',
                 accent: '#82B1FF',
                 error: '#FF5252',
                 info: '#2196F3',
+                primary: '#007BFF',
+                secondary: '#424242',
                 success: '#4CAF50',
                 warning: '#FFC107',
+                white: '#FFFFFF'
             }
         }
     }
