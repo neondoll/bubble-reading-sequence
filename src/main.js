9ff4763a1b81a467cd0c1b62f114484e9d0a30df
neondoll <bonni_1998@mail.ru>
2024-02-09 19:35:51 +0300

    feat: Update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import {createApp} from "vue";
-import App from "./App.vue";
-import router from "./router.js";
-import "./new_style.css";
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router.js';
+import './css/new_style.css';
 
 const app = createApp(App);
 app.use(router);
-app.mount("#app");
+app.mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,27 +1,26 @@
-import {RouterView, createRouter, createWebHashHistory} from "vue-router";
-import {h} from "vue";
-import Author from "./pages/Author.vue";
-import Authors from "./pages/Authors.vue";
-import BubbleUniverseMap from "./pages/BubbleUniverseMap.vue";
-import Character from "./pages/Character.vue";
-import Characters from "./pages/Characters.vue";
-import Comic from "./pages/Comic.vue";
-import Comics from "./pages/Comics.vue";
-import ComicsRange from "./pages/ComicsRange.vue";
-import Home from "./pages/Home.vue";
+import {RouterView, createRouter, createWebHashHistory} from 'vue-router';
+import {h} from 'vue';
 
 const routes = [
-  {path: "/", name: "home", component: Home},
-  {path: "/author/:authorId", name: "author", component: Author},
-  {path: "/authors", name: "authors", component: Authors},
-  {path: "/bubble-universe-map", name: "bubbleUniverseMap", component: BubbleUniverseMap},
-  {path: "/characters", name: "characters", component: Characters},
-  {path: "/characters/:characterId", name: "character", component: Character},
+  {path: '/', name: 'home', component: () => import('./pages/Home.vue')},
   {
-    path: "/comics", component: {render: () => h(RouterView)}, children: [
-      {path: "", name: "comics", component: Comics},
-      {path: ":rangeId", name: "comicsRange", component: ComicsRange},
-      {path: ":rangeId/:comicId", name: "comic", component: Comic}
+    path: '/authors', component: {render: () => h(RouterView)}, children: [
+      {path: '', name: 'authors', component: () => import('./pages/Authors.vue')},
+      {path: ':authorId', name: 'author', component: () => import('./pages/Author.vue')}
+    ]
+  },
+  {path: '/bubble-universe-map', name: 'bubbleUniverseMap', component: () => import('./pages/BubbleUniverseMap.vue')},
+  {
+    path: '/characters', component: {render: () => h(RouterView)}, children: [
+      {path: '', name: 'characters', component: () => import('./pages/Characters.vue')},
+      {path: ':characterId', name: 'character', component: () => import('./pages/Character.vue')}
+    ]
+  },
+  {
+    path: '/comics', component: {render: () => h(RouterView)}, children: [
+      {path: '', name: 'comics', component: () => import('./pages/Comics.vue')},
+      {path: ':rangeId', name: 'comicsRange', component: () => import('./pages/ComicsRange.vue')},
+      {path: ':rangeId/:comicId', name: 'comic', component: () => import('./pages/Comic.vue')}
     ]
   }
 ];
